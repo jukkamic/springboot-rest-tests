cbefc7642f87df4c4a4b4ad005a2799309a88507
Jukka Michelsson <jukkamic@gmail.com>
2025-12-13 19:26:18 +0200

    Added logging

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -5,9 +5,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 public class GreetingController {
+
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
 	private final AtomicLong counter = new AtomicLong();
 
@@ -19,6 +22,8 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+		logger.info("Received greeting request for name: {}", name);
+		logger.debug("Logger class: {}", logger.getClass().getName());
 		String greetingMessage = greetingService.greet(name);
         return new Greeting(counter.incrementAndGet(), greetingMessage);
 	}
--- src/main/java/com/example/restservice/GreetingService.java
+++ src/main/java/com/example/restservice/GreetingService.java
@@ -1,9 +1,17 @@
 package com.example.restservice;
 
 import org.springframework.stereotype.Service;
-@Service 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
 public class GreetingService {
+    private static final Logger logger = LoggerFactory.getLogger(GreetingService.class);
+
     public String greet(String name) {
+        logger.info("Greeting user: {}", name);
+        logger.debug("Logger class: {}", logger.getClass().getName());
+
         return "Hello, " + name + "!";
     }
 }
